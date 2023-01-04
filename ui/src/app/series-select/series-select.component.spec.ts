df8db07da8c00db88aa6936b4f15cbd42f2c8017
PeteLeeds <peter_clark@talktalk.net>
2023-01-04 22:23:37 +0000

    Test series-select-component

--- ui/src/app/series-select/series-select.component.spec.ts
+++ ui/src/app/series-select/series-select.component.spec.ts
@@ -1,19 +1,40 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router, Routes } from '@angular/router';
 import { RouterTestingModule } from "@angular/router/testing";
+import { of } from 'rxjs';
+import { ChartService } from '../services/chart.service';
 
 import { SeriesSelectComponent } from './series-select.component';
+
+@Component({})
+class TestChartDisplayComponent {}
+
+const routes: Routes = [
+  { path: 'series/:id/chart', component: TestChartDisplayComponent }
+];
 
 describe('SeriesSelectComponent', () => {
   let component: SeriesSelectComponent;
   let fixture: ComponentFixture<SeriesSelectComponent>;
+  let mockChartService: ChartService;
+  let router: Router;
 
   beforeEach(async () => {
+    mockChartService = jasmine.createSpyObj<ChartService>(
+      'ChartService',
+      {
+        getSeries: of([{id: '0', name: 'Test Series', charts: []}])
+      }
+    );
     await TestBed.configureTestingModule({
       declarations: [ SeriesSelectComponent ],
-      imports: [ HttpClientTestingModule, RouterTestingModule ],
+      imports: [ HttpClientTestingModule, RouterTestingModule.withRoutes(routes) ],
+      providers: [{provide: ChartService, useValue: mockChartService}]
     })
     .compileComponents();
+    router = TestBed.inject(Router);
   });
 
   beforeEach(() => {
@@ -25,4 +46,18 @@ describe('SeriesSelectComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain charts passed in', () => {
+    const link = fixture.nativeElement.querySelector('a')
+    expect(link.textContent).toContain('Test Series');
+  });
+
+  it('should route to chart component correctly', fakeAsync(() => {
+    const navigateSpy = spyOn(router, 'navigateByUrl');
+    const link = fixture.nativeElement.querySelector('a')
+    link.click()
+    tick(1)
+    const calledUrl = navigateSpy.calls.first().args[0].toString()
+    expect(calledUrl).toContain('/series/Test%20Series/chart');
+  }));
 });
