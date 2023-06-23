98f99e9c2a63c57c6ed8c1fb9a11ee318365896f
PeteLeeds <peter_clark@talktalk.net>
2023-06-23 21:19:19 +0100

    add search functionality to song component

--- ui/src/app/song/song.component.ts
+++ ui/src/app/song/song.component.ts
@@ -16,6 +16,7 @@ export class SongComponent implements OnInit {
   public sortBy = "title";
   public sortOptions = ["title", "artistDisplay"];
   public pageNumber = 1;
+  public searchParams = {}
 
   constructor(songService: SongService) {
     this.songService = songService;
@@ -25,8 +26,15 @@ export class SongComponent implements OnInit {
     this.reloadSongs();
   }
 
+  public setParams(params: Record<string, string>) {
+    this.searchParams = params
+    this.pageNumber = 1
+    this.reloadSongs()
+  }
+
   public reloadSongs() {
     const queryOptions = {
+      ...this.searchParams,
       sortBy: this.sortBy,
       pageNumber: (this.pageNumber - 1).toString(),
       limit: "20"
