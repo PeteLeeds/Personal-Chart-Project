e0698935201b7c17b6452c1dce2a125538e4ba80
PeteLeeds <peter_clark@talktalk.net>
2024-12-27 18:35:43 +0000

    amend testbed for finalise component

--- ui/src/app/create-chart-finalise/create-chart-finalise.component.spec.ts
+++ ui/src/app/create-chart-finalise/create-chart-finalise.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateChartFinaliseComponent } from './create-chart-finalise.component';
+import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 describe('CreateChartFinaliseComponent', () => {
   let component: CreateChartFinaliseComponent;
   let fixture: ComponentFixture<CreateChartFinaliseComponent>;
 
+  const mockActivatedRoute = {
+    params: of({ session: 'test' }),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateChartFinaliseComponent ]
+      providers: [
+        {provide: ActivatedRoute, useValue: mockActivatedRoute},
+      ], 
+      declarations: [ CreateChartFinaliseComponent ],
+      imports: [HttpClientModule],
     })
     .compileComponents();
 
