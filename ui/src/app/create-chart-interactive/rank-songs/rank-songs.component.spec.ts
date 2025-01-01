9c08bbc91c5093594d79d9b8b23616cc97c1b42c
PeteLeeds <peter_clark@talktalk.net>
2025-01-01 19:42:25 +0000

    Destroy fixture after each test

--- ui/src/app/create-chart-interactive/rank-songs/rank-songs.component.spec.ts
+++ ui/src/app/create-chart-interactive/rank-songs/rank-songs.component.spec.ts
@@ -1,35 +1,39 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { RankSongsComponent } from './rank-songs.component';
-// import { of } from 'rxjs';
-// import { ActivatedRoute } from '@angular/router';
-// import { HttpClientModule } from '@angular/common/http';
+import { RankSongsComponent } from './rank-songs.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-// describe('RankSongsComponent', () => {
-//   let component: RankSongsComponent;
-//   let fixture: ComponentFixture<RankSongsComponent>;
+describe('RankSongsComponent', () => {
+  let component: RankSongsComponent;
+  let fixture: ComponentFixture<RankSongsComponent>;
 
-//   const mockActivatedRoute = {
-//     params: of({ session: 'test' }),
-//   };
+  const mockActivatedRoute = {
+    params: of({ session: 'test' }),
+  };
   
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       providers: [
-//         {provide: ActivatedRoute, useValue: mockActivatedRoute},
-//       ], 
-//       imports: [HttpClientModule],
-//       declarations: [ RankSongsComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        {provide: ActivatedRoute, useValue: mockActivatedRoute},
+      ], 
+      imports: [HttpClientModule],
+      declarations: [ RankSongsComponent ]
+    })
+    .compileComponents();
 
-//     fixture = TestBed.createComponent(RankSongsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(RankSongsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  afterEach(() => {
+    fixture.destroy();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
