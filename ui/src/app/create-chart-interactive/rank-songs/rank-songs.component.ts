18ef88b1e171ff0f2d427697ff3519744655ae83
PeteLeeds <peter_clark@talktalk.net>
2025-01-02 16:39:27 +0000

    add back in everything except injected stuff

--- ui/src/app/create-chart-interactive/rank-songs/rank-songs.component.ts
+++ ui/src/app/create-chart-interactive/rank-songs/rank-songs.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 // import { of } from 'rxjs';
 // import { mergeMap } from 'rxjs/operators';
 // import { ChartService } from 'src/app/services/chart.service';
+import { Session } from 'src/app/types/chart';
 // import { Session, SessionSong } from 'src/app/types/chart';
 // import { faListOl, faPlus } from '@fortawesome/free-solid-svg-icons';
 // import { AddSongsComponent } from 'src/app/modals/add-songs/add-songs.component';
@@ -19,11 +20,11 @@ export class RankSongsComponent {
     // private activatedRoute: ActivatedRoute
     // private chartService: ChartService
 
-    // public sessionId: string
-    // public session: Session
-    // public insertButtonsToDisplay: number[]
-    // public songToMove: number
-    // public showNumbers = false
+    public sessionId: string
+    public session: Session
+    public insertButtonsToDisplay: number[]
+    public songToMove: number
+    public showNumbers = false
 
     // faListOl = faListOl
     // faPlus = faPlus
@@ -45,58 +46,58 @@ export class RankSongsComponent {
     //   })
     // }
 
-    // private updateCurrentSession() {
+    private updateCurrentSession() {
     //   this.chartService.updateSession(this.sessionId, {
     //     placedSongs: this.session.placedSongs,
     //     songOrder: this.session.songOrder
     //   }).subscribe()
-    // }
+    }
 
-    // public addSong(position: number): void {
-    //   if (this.songToMove != null) {
-    //     const movingSong = this.session.placedSongs[this.songToMove]
-    //     this.session.placedSongs[this.songToMove] = null
-    //     this.session.placedSongs.splice(position, 0, movingSong)
-    //     this.session.placedSongs = this.session.placedSongs.filter(song => !!song)
-    //     this.songToMove = null
-    //   } else {
-    //     this.session.placedSongs.splice(position, 0, this.session.songOrder[0])
-    //     this.session.songOrder.shift()
-    //   }
-    //   this.updateCurrentSession()
-    // }
+    public addSong(position: number): void {
+      if (this.songToMove != null) {
+        const movingSong = this.session.placedSongs[this.songToMove]
+        this.session.placedSongs[this.songToMove] = null
+        this.session.placedSongs.splice(position, 0, movingSong)
+        this.session.placedSongs = this.session.placedSongs.filter(song => !!song)
+        this.songToMove = null
+      } else {
+        this.session.placedSongs.splice(position, 0, this.session.songOrder[0])
+        this.session.songOrder.shift()
+      }
+      this.updateCurrentSession()
+    }
 
-    // public setInsertButtonsToDisplay(valueHoveredOver: number): void {
-    //   this.insertButtonsToDisplay = [valueHoveredOver - 1, valueHoveredOver]
-    // }
+    public setInsertButtonsToDisplay(valueHoveredOver: number): void {
+      this.insertButtonsToDisplay = [valueHoveredOver - 1, valueHoveredOver]
+    }
 
-    // public removeInsertButtons(): void {
-    //   this.insertButtonsToDisplay = []
-    // }
+    public removeInsertButtons(): void {
+      this.insertButtonsToDisplay = []
+    }
 
-    // public selectSongToMove(position: number): void {
-    //   if (this.songToMove == position) {
-    //     this.songToMove = null
-    //   } else {
-    //     this.songToMove = position
-    //   }
-    //   console.log(this.songToMove)
-    // }
+    public selectSongToMove(position: number): void {
+      if (this.songToMove == position) {
+        this.songToMove = null
+      } else {
+        this.songToMove = position
+      }
+      console.log(this.songToMove)
+    }
 
-    // public moveToBack(): void {
-    //   this.session.songOrder.push(this.session.songOrder[0])
-    //   this.session.songOrder.shift()
-    //   this.updateCurrentSession()
-    // }
+    public moveToBack(): void {
+      this.session.songOrder.push(this.session.songOrder[0])
+      this.session.songOrder.shift()
+      this.updateCurrentSession()
+    }
 
-    // public toggleNumbers(): void {
-    //   this.showNumbers = !this.showNumbers
-    // }
+    public toggleNumbers(): void {
+      this.showNumbers = !this.showNumbers
+    }
 
-    // public async openAddSongsModal(): Promise<void> {
-    //   let newSongs = await this.addSongsModal.open() as SessionSong[]
-    //   this.session.songOrder.push(...newSongs)
-    //   this.updateCurrentSession()
-    // }
+    public async openAddSongsModal(): Promise<void> {
+      // let newSongs = await this.addSongsModal.open() as SessionSong[]
+      // this.session.songOrder.push(...newSongs)
+      // this.updateCurrentSession()
+    }
 
 }
